e630fb6b1154163bed00368ee6191da8dd9d548c
Somtochukwu <you@example.com>
2022-12-02 12:55:58 +0000

    Work in Progress

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,76 +1,3 @@
-/**********************************
- *
- *
- * INITIALIZE ALL DOM and Attach event Listeners
- *
- *
- * **********************************
- */
-
-class DomAcessor {
-  #dom = document;
-  #eventName = null;
-  elem = null;
-
-  query(selector, all = false) {
-    this.elem = all
-      ? this.#dom.querySelectorAll(selector)
-      : this.#dom.querySelector(selector);
-    return this;
-  }
-  event(eventName) {
-    this.#eventName = eventName;
-    return this;
-  }
-
-  listen(cb) {
-    if (this.elem instanceof NodeList) {
-      this.elem.forEach((el) => {
-        this.#attach(el, cb);
-      });
-    } else {
-      this.#attach(this.elem, cb);
-    }
-
-    return this;
-  }
-
-  #attach(el, cb) {
-    if (!this.#eventName) {
-      throw Error("no event attached");
-    }
-    el.addEventListener(this.#eventName, cb);
-  }
-
-  remove(selector) {
-    if (!selector) {
-      if (this.elem instanceof NodeList) {
-        this.elem.forEach((el) => el.remove());
-      } else {
-        this.elem.remove();
-      }
-    } else {
-      const elem = document.querySelectorAll(selector);
-      elem.forEach((el) => el.remove());
-    }
-  }
-}
-
-function query(selector) {
-  return document.querySelector(selector);
-}
-
-function queryAll(selector) {
-  return document.querySelectorAll(selector);
-}
-
-/***
- *
- *
- * Todo Class
- *
- */
-
 class Todo {
   todos = [];
 
@@ -78,6 +5,7 @@ class Todo {
     const todoId = Math.floor(Math.random() * 1 * Date.now()).toString(32);
     const todo = { title, id: todoId };
     this.todos.push(todo);
+
     return todo;
   }
 
@@ -90,19 +18,6 @@ class Todo {
     });
   }
 
-  edit(el) {
-    let liContent = el.querySelector("span");
-    let input = document.createElement("input");
-    input.type = "text";
-    input.value = liContent.textContent;
-    el.append(input);
-    input.focus();
-    input.addEventListener("blur", () => {
-      liContent.innerHTML = input.value;
-      input.remove();
-    });
-  }
-
   removeAll(el) {
     this.todos = [];
     el.innerHTML = null;
@@ -110,23 +25,20 @@ class Todo {
 }
 
 const todo = new Todo();
-/**
- *
- *
- * Update UI
- *
- */
+
 function addTodoUI(todo) {
-  const todoUI = `<div class="todo-cont">
-            <input type="checkbox" name="" id="todo-${todo.id}" />
-            <label for="todo-${todo.id}">
-              ${todo.title}
-            </label>
-          </div>
-          <div class="todo-actions">
-            <button class='"todo-ed-btn-${todo.id}"'>edit</button>
-            <button class="todo-rm-btn-${todo.id}">delete</button>
-            </div>
+  const todoUI = `
+    <div class="todo-cont">
+      <input type="checkbox" name="" id="todo-${todo.id}" />
+      <label for="todo-${todo.id}">
+        ${todo.title}
+      </label>
+    </div>
+          
+    <div class="todo-actions">
+      <button class="todo-ed-btn-${todo.id}">edit</button>
+      <button class="todo-rm-btn-${todo.id}">delete</button>
+    </div>
         `;
 
   const li = document.createElement("li");
@@ -140,7 +52,10 @@ function addTodoUI(todo) {
   const todos = query(".todo-list");
   todos.appendChild(domFragment);
   attachRemoveTodo(query(`.todo-rm-btn-${todo.id}`), todo.id);
-    console.log(query(`.todo-ed-btn-${todo.id}`));
+
+  attachEditTodo(query(`.todo-ed-btn-${todo.id}`));
+
+  checkBox(`#todo-${todo.id}`)
 }
 
 function attachRemoveTodo(el, id) {
@@ -150,32 +65,59 @@ function attachRemoveTodo(el, id) {
   });
 }
 
-/**
- *
- * Handle Event
- */
+function attachEditTodo(el) {
+  el.addEventListener("click", () => {
+    const label = el.parentElement.previousElementSibling.lastElementChild;
 
-window.onload = (e) => {
-  const todoAddBtn = query(".todo-add-btn");
+    if (el.innerText.toLowerCase() === "edit") {
+      label.contentEditable = true;
+      el.innerText = "save";
+    } else {
+      label.contentEditable = false;
+      el.innerText = "edit";
+    }
+  });
+}
+
+
+
+
+function query(selector) {
+  return document.querySelector(selector);
+}
+
+function queryAll(selector) {
+  return document.querySelectorAll(selector);
+}
+
+window.addEventListener("load", () => {
+  const form = query(".todo-form");
+  const input = query(".todo-title");
   const todos = query(".todo-list");
-  const todoTitle = query(".todo-title");
   const clearAllBtn = query(".clear-all-btn");
+  const pending = query(`[data-selection='pending']`);
+  form.onsubmit = (e) => {
+    e.preventDefault();
 
-  todoAddBtn.addEventListener("click", () => {
-    // get the todo text title
-    const title = todoTitle.value;
+    const title = input.value;
     if (!title || title.trim() === "") {
       return false;
     }
-    // add to array object
+
+    // adds the todo value
     const addedTodo = todo.addTodo(title);
 
     // update the textarea value
-    todoTitle.value = null;
+    input.value = null;
 
     // display todo to the UI
     addTodoUI(addedTodo);
-  });
+  };
 
   clearAllBtn.addEventListener("click", () => todo.removeAll(todos));
-};
+
+  pending.onclick = () => {
+    pending.classList.add('active')
+    // pending.classList.add("active");
+  };
+});
